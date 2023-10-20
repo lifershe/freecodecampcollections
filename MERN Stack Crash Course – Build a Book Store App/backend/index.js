95804750d53411a573b488606c566bcdd5236463
baisreycarlo <bais.reycarlo@gmail.com>
2023-10-20 22:28:02 +0800

    update

--- MERN Stack Crash Course – Build a Book Store App/backend/index.js
+++ MERN Stack Crash Course – Build a Book Store App/backend/index.js
@@ -10,6 +10,19 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling cors policy
+//Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+
+// //Option 2: Allow Custom Origins
+// app.use(
+//     cors({
+//         origin: 'http://localhost:5173',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHGeaders: ['Content-Type'],
+//     })
+// );
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN stack');
--- MERN Stack Crash Course – Build a Book Store App/frontend/Mern sample/src/App.jsx
+++ MERN Stack Crash Course – Build a Book Store App/frontend/Mern sample/src/App.jsx
@@ -1,18 +1,20 @@
-import { useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
-function App() {
-  useEffect(() => {
-    axios.get('http://localhost:5555/books')
-      .then(response => console.log(response)
-    )
-  }, [])
-
+const App = () => {
   return (
-    <div>
-      <h1>A simple Cors policy</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={ <Home/>} />
+      <Route path='/books/create' element={<CreateBook/> } />
+      <Route path='/books/details/:id' element={<ShowBook/> } />
+      <Route path='/books/edit/:id' element={<EditBook/> } />
+      <Route path='/books/delete/:id' element={<DeleteBook/> } />
+    </Routes>
   )
 }
 
