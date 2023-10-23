6a1b781ff419f1e2641582260ece40b79cf023dc
baisreycarlo <bais.reycarlo@gmail.com>
2023-10-23 16:55:39 +0800

    final

--- MERN Stack Crash Course – Build a Book Store App/frontend/Mern sample/src/pages/ShowBook.jsx
+++ MERN Stack Crash Course – Build a Book Store App/frontend/Mern sample/src/pages/ShowBook.jsx
@@ -1,64 +1,64 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
-import BackButton from '../components/BackButton'
-import Spinner from '../components/Spinner'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 const ShowBook = () => {
-  const [book, setBook] = useState({})
-  const [loading, setLoading] = useState(false)
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     axios
       .get(`http://localhost:5555/books/${id}`)
       .then((response) => {
-        setBook(response.data)
-        setLoading(false)
+        setBook(response.data);
+        setLoading(false);
       })
       .catch((erro) => {
         console.log(error);
-        setLoading(false)
-    })
-  }, [])
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <BackButton />
-      <h1 className='text-3x1 my-4'>Show Book</h1>
+      <h1 className="text-3x1 my-4">Show Book</h1>
       {loading ? (
-      <Spinner/>
-      ): (
-          <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
-            <div className='my-4'>
-              <span className='text-xl mr-4 text-gray-500'>Id</span>
-              <span>{book._id}</span>
-            </div>
-            <div className='my-4'>
-              <span className='text-xl mr-4 text-gray-500'>Title</span>
-              <span>{book.title}</span>
-            </div>
-             <div className='my-4'>
-              <span className='text-xl mr-4 text-gray-500'>Author</span>
-              <span>{book.author}</span>
-            </div>
-             <div className='my-4'>
-              <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
-              <span>{book.publishYear}</span>
-            </div>
-              <div className='my-4'>
-              <span className='text-xl mr-4 text-gray-500'>Created Time</span>
-              <span>{book.createdAt}</span>
-            </div>
-              <div className='my-4'>
-              <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
-              <span>{book.updatedAt}</span>
-            </div>
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Id</span>
+            <span>{book._id}</span>
           </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Publish Year</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Created Time</span>
+            <span>{book.createdAt}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
+            <span>{book.updatedAt}</span>
+          </div>
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default ShowBook
+export default ShowBook;
