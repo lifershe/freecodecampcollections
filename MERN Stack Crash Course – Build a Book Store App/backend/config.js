8bfc6e7f373cbec297fa9285575da16a9f4bb173
lifershe <115638983+lifershe@users.noreply.github.com>
2023-10-23 20:02:12 +0800

    Update config.js

--- MERN Stack Crash Course – Build a Book Store App/backend/config.js
+++ MERN Stack Crash Course – Build a Book Store App/backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-    'mongodb+srv://root:root@react-mern-sample.oqigeqi.mongodb.net/sampledb?retryWrites=true&w=majority'
+    'mongodb+srv://root:root@react-mern-sample.#####.mongodb.net/sampledb?retryWrites=true&w=majority'
